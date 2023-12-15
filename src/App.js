225205709fb4294d2793200be8296dbbbf143fb4
Phuoc Hong Vu <phuocvu511@gmail.com>
2023-12-15 16:18:40 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useRoutes } from "react-router-dom";
+import SignInSide from "./components/SignInSide";
+import SignUp from "./components/SignUp";
+import Root from "./routes/Root";
+import ErrorPage from "./components/ErrorPage";
+import Main from "./components/Main";
+import { ProtectedRoute } from "./authentication/ProtectedRoute";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+export default function App() {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Root></Root>,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/signin",
+      element: <SignInSide></SignInSide>,
+    },
+    {
+      path: "/signup",
+      element: <SignUp></SignUp>,
+    },
+    {
+      path: "/main",
+      element: (
+        <ProtectedRoute>
+          <Main></Main>
+        </ProtectedRoute>
+      ),
+    },
+  ]);
+  return routes;
 }
-
-export default App;
